a1b7e5c1d67a06938c3184c384c338228b3a4e12
QuocTuanJV <quoctuan.naf@gmail.com>
2019-12-27 15:45:27 +0700

    init

---
+++ src/test/java/code/team/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package code.team.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
